0d445935a51e3cd9a454fd49997339727bc87d20
Deng Balaga <dengbalaga@gmail.com>
2019-11-25 08:44:27 +0800

    testcommit by branch
    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactccccccc
+          Learn Reactaaaaaaaaaaaaa
         </a>
       </header>
     </div>
