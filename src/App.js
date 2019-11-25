6a04b46ecbe390897d6b2b25e71ca25cd7b860c4
jhfelizardo12 <58068601+jhfelizardo12@users.noreply.github.com>
2019-11-25 08:52:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaaaaaaaaaaaaa
+          Learn Reactssssssz
         </a>
       </header>
     </div>
